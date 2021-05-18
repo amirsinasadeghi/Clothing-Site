73c42f0f95e62643f17dc6cb696086ccec16626f
19770 <sadeghi.amirsina@gmail.com>
2021-05-18 14:26:31 +1200

    changed the color and background of text in nav and footer and added more details to the clothing page

--- routes.py
+++ routes.py
@@ -16,15 +16,16 @@ def clothing (id):
   cursor = conn.cursor()
   cursor.execute("SELECT * FROM Clothes WHERE id=?;", (id,))
   clothing = cursor.fetchone()
+  cursor.execute("SELECT name FROM Brands WHERE id IN (SELECT brandid FROM ClothesBrand WHERE clothesid = (SELECT id FROM Clothes WHERE name = ?))", (clothing[1],))
+  brand = cursor.fetchall() 
   conn.close()
   print(clothing)
-  return render_template('clothing.html', clothing=clothing )
+  return render_template('clothing.html', clothing=clothing, brand=brand )
 
 
 @app.route('/about')
 def about():
   return render_template('about.html', title="about") 
-
 
 if __name__ == "__main__" :
   app.run(debug=True)
