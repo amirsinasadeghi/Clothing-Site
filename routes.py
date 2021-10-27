74e4d4c8e5aa94c3b108f2bf7e53b38d76eff3f9
amirsinasadeghi <sadeghi.amirsina@gmail.com>
2021-10-28 11:53:35 +1300

    added grid and comment

--- routes.py
+++ routes.py
@@ -5,24 +5,26 @@ import sqlite3
 app = Flask(__name__)
 
 
-@app.errorhandler(404) 
-def invalid_route(e): 
+@app.errorhandler(404)
+def invalid_route(e):
   return render_template('errorpage.html', title="404errorpage")
 
 
 # this is the route for my home page.
 @app.route('/')
-def home ():
+def home():
   return render_template('home.html', title="home")
 
-#this takes you to a page which allows the user to contact the creator
+
+# this takes you to a page which allows the user to contact the creator
 @app.route('/about')
 def about():
-  return render_template('about.html', title="about") 
+  return render_template('about.html', title="about")
 
 
-#this simplifies any query that im doing so that I dont need to rewrite the thing out all the time.
-def do_query (query, data=None, fetchone=False):
+# this simplifies any query that im doing
+# this is so that I dont need to rewrite the thing out all the time.
+def do_query(query, data=None, fetchone=False):
   conn = sqlite3.connect("clothing.db")
   cur = conn.cursor()
   if data is None:
@@ -34,34 +36,38 @@ def do_query (query, data=None, fetchone=False):
   return results
 
 
-#This query grabs all the data from anything that classifies as a shirt (id of 2) and displays it, after this users can select on one specific shirt name and it will take them to the link of the ID of the shirt.
-@app.route ('/shirts')
-def shirts ():
-  shirts = do_query ('SELECT id,name,price,photo FROM Clothes WHERE typeid="2"')
-  return render_template('shirts.html', shirts=shirts )
+# This query grabs all the data from anything that classifies as a shirt
+# (id of 2) and displays it after this users can select on one specific
+# shirt name and it will take them to the link of the ID of the shirt.
+@app.route('/shirts')
+def shirts():
+  shirts = do_query('SELECT id, name, price, photo FROM Clothes WHERE typeid="2"')
+  return render_template('shirts.html', shirts=shirts)
 
 
-#this query grabs the anything from the table named pants and displays its id, price and photo
-@app.route ('/pants')
-def pants ():
-  pants = do_query ('SELECT id,name,price,photo FROM Clothes WHERE typeid="1"')
-  return render_template('pants.html', pants=pants )
+# this query grabs the anything from the table named pants and
+# displays its id, price and photo
+@app.route('/pants')
+def pants():
+  pants = do_query('SELECT id,name,price,photo FROM Clothes WHERE typeid="1"')
+  return render_template('pants.html', pants=pants)
 
 
-#this query does the same thing as the pants query but for anything that classifies as sweater
-@app.route ('/sweaters')
-def sweaters ():
-  sweaters = do_query ('SELECT id,name,price,photo FROM Clothes WHERE typeid="3"')
-  return render_template('sweaters.html', sweaters=sweaters )
+# this query does the same thing as the pants query
+# but for anything that classifies as sweater
+@app.route('/sweaters')
+def sweaters():
+  sweaters = do_query('SELECT id,name,price,photo FROM Clothes WHERE typeid="3"')
+  return render_template('sweaters.html', sweaters=sweaters)
 
 
-#This grabs all the info for one specific clothing item and displays it onto the screen.
-@app.route ('/clothingitem/<int:id>')
-def clothingitem (id):
-  clothingitem = do_query (" SELECT * FROM Clothes WHERE id=?;", (id,), fetchone=True)
-  color = do_query('SELECT * FROM Color WHERE id IN (SELECT colorid FROM ClothesColor WHERE clothesid=?)',(id,), fetchone = False) 
-  return render_template('clothingitem.html',clothingitem=clothingitem,color=color )
+# This grabs all the info for one specific clothing item and displays it onto the screen.
+@app.route('/clothingitem/<int:id>')
+def clothingitem(id):
+  clothingitem = do_query(" SELECT * FROM Clothes WHERE id=?;", (id,), fetchone=True)
+  color = do_query('SELECT * FROM Color WHERE id IN (SELECT colorid FROM ClothesColor WHERE clothesid = ?)', (id,), fetchone=False) 
+  return render_template('clothingitem.html', clothingitem=clothingitem, color=color)
 
 
-if __name__ == "__main__" :
+if __name__ == "__main__":
   app.run(debug=True)
